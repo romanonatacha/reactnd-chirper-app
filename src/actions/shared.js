ff97009dd322107fab76ba1673401dbe85511da0
Natacha Romano <natacharomanonr@gmail.com>
2019-03-06 18:31:44 -0300

    initial data v2

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -5,7 +5,7 @@ import { setAuthedUser } from '../actions/authedUser'
 
 const AUTHED_ID = 'tylermcginnis'
 
-export function handleIitialData () {
+export function handleInitialData () {
     return (dispatch) => {
         return getInitialData()
             .then(({ users, tweets }) => {
--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -3,7 +3,7 @@ const logger = (store) => (next) => (action) => {
         console.log('The action: ', action)
         const returnValue = next(action)
         console.log('The new state: ', store.getState())
-    console,groupEnd()
+    console.groupEnd()
     return returnValue
 }
 
