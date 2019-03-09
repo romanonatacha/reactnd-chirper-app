113793612d0a7e3748b18ee71d036d7c3f097137
natacha romano <romano.n@hotmail.com>
2019-03-09 20:21:45 -0300

    Updates

---
+++ precache-manifest.33916c419c44d742ad1cd1fa54c2fc7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c296c0caa6dbbce45cb",
+    "url": "/reactnd-chirper-app/static/css/main.de995996.chunk.css"
+  },
+  {
+    "revision": "8c296c0caa6dbbce45cb",
+    "url": "/reactnd-chirper-app/static/js/main.8c296c0c.chunk.js"
+  },
+  {
+    "revision": "c56381d46d399d1eee0e",
+    "url": "/reactnd-chirper-app/static/js/runtime~main.c56381d4.js"
+  },
+  {
+    "revision": "1869a9ccad2c2fce5495",
+    "url": "/reactnd-chirper-app/static/js/2.1869a9cc.chunk.js"
+  },
+  {
+    "revision": "d384454de748f983c28cb9c9b45085b6",
+    "url": "/reactnd-chirper-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/reactnd-chirper-app/precache-manifest.33916c419c44d742ad1cd1fa54c2fc7e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/reactnd-chirper-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
