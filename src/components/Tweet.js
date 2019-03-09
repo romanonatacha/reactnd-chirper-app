bd25c8dd164b04559a62bbf0c7237cae351ea230
Natacha Romano <natacharomanonr@gmail.com>
2019-03-09 20:18:42 -0300

    react router

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,7 @@ import { TiArrowBackOutline } from 'react-icons/ti/index'
 import { TiHeartOutline } from 'react-icons/ti/index'
 import { TiHeartFullOutline } from 'react-icons/ti/index'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
 
@@ -22,7 +23,7 @@ class Tweet extends Component {
 
     toParent = (e, id) => {
         e.preventDefault()
-        // todo: redirect to parent tweet
+       this.props.history.push(`/tweet/${id}`)
     }
 
     render() {
@@ -32,10 +33,10 @@ class Tweet extends Component {
             return <p>This tweet doesn't exist.</p>
         }
 
-        const { name, avatar, timestamp, text, hasLiked, likes, replies, parent } = tweet
+        const { name, avatar, timestamp, text, hasLiked, likes, replies, parent, id  } = tweet
 
         return (
-            <div className='tweet'>
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img src={avatar} alt={`Avatar of ${name}`} className='avatar' />
                 <div className='tweet-info'>
                     <div>
@@ -59,7 +60,7 @@ class Tweet extends Component {
                         <span>{likes !== 0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -76,4 +77,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
